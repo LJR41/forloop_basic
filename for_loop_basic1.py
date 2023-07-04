78f8ef15559fef7b14a11e327d59f2c605a5393f
LJR411~ <ljremulla@yahoo.com>
2023-07-03 17:40:45 -0700

    initcommit

---
+++ for_loop_basic1.py
@@ -0,0 +1,33 @@
+# for i in range(151):
+#     print(i)
+
+# for i in range(5,1005,5):
+#     print(i)
+
+# for i in range(1,100,1):
+#     if i % 10 == 0:
+#         print("Coding Dojo")
+#         continue
+#     if i % 5 == 0:
+#         print("Coding")
+#         continue
+#     print(i)
+
+
+# odd_sum = 0
+# for i in range(0,500000,1):
+#     if i % 2 == 1:
+#         odd_sum += i
+    
+# print(odd_sum)
+
+# for i in range(2018, 0, -4):
+#     print(i)
+
+low_num = 1
+high_num = 37
+mult = 13
+
+for i in range(low_num,high_num,1):
+    if i % mult == 0:
+        print(i)
